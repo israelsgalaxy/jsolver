ec0febdfd2f01438c48452df04f5d448bb7c4a0a
Israel Galadima <izzygaladima@gmail.com>
2025-08-22 19:43:03 +0100

    Init

---
+++ main.js
@@ -0,0 +1,374 @@
+import express from 'express';
+import { Cluster } from "patchright-browserpool-cluster";
+import Database from 'better-sqlite3';
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
+
+const argv = yargs(hideBin(process.argv))
+    .option("max_concurrency", {
+        alias: "c",
+        type: "number",
+        describe: "Maximum number of concurrent workers",
+        default: 30,
+    })
+    .option("worker_timeout", {
+        alias: "t",
+        type: "number",
+        describe: "Worker timeout in ms",
+        default: 100 * 1000,
+    })
+    .option("worker_creation_delay", {
+        alias: "d",
+        type: "number",
+        describe: "Delay between worker creation in ms",
+        default: 2000,
+    })
+    .option("browser_type", {
+        alias: "b",
+        choices: ["chrome"],
+        describe: "Browser type to use",
+        default: "chrome",
+    })
+    .option("headless", {
+        alias: "H",
+        type: "boolean",
+        describe: "Run browser in headless mode",
+        default: false,
+    })
+    .option("port", {
+        alias: "p",
+        type: "number",
+        describe: "Port to run the service on",
+        default: 80,
+    })
+    .option("sqlite_timeout", {
+        type: "number",
+        describe: "SQLite query timeout in milliseconds",
+        default: 30000,
+    })
+    .option("proxy_domain", {
+        type: "string",
+        describe: "Proxy domain or IP address",
+        demandOption: true,
+    })
+    .option("proxy_port", {
+        type: "number",
+        describe: "Proxy port",
+        demandOption: true,
+    })
+    .option("proxy_password", {
+        type: "string",
+        describe: "Proxy password",
+        demandOption: true,
+    })
+    .option("proxy_protocol", {
+        type: "string",
+        choices: ["http", "https", "socks4", "socks5"],
+        describe: "Proxy protocol to use",
+        default: "http",
+    })
+    .option("proxy_username", {
+        type: "string",
+        describe: "Proxy username",
+        demandOption: true,
+    })
+    .option("proxy_sid_length", {
+        type: "number",
+        describe: "Length of proxy session ID",
+        default: 8,
+    })
+    .strict()
+    .help()
+    .parse();
+
+const db = new Database('captcha_results.db', {timeout: argv.sqlite_timeout});
+db.pragma('journal_mode = WAL');
+
+function randomString(length) {
+    const chars = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    let result = '';
+    for (let i = 0; i < length; i++) {
+        result += chars.charAt(Math.floor(Math.random() * chars.length));
+    }
+    return result;
+}
+
+const cluster = await Cluster.launch({
+    concurrency: Cluster.CONCURRENCY_CONTEXT,
+    maxConcurrency: argv.max_concurrency,
+    timeout: argv.worker_timeout,
+    monitor: false,
+    workerCreationDelay: argv.worker_creation_delay,
+    launchOptions: {
+        channel: argv.browser_type,
+        headless: argv.headless
+    },
+    contextOptions: {
+        proxyGenerator: () => {
+            const protocol = "http";
+            const username = argv.proxy_username.replace("${sid}", randomString(argv.proxy_sid_length));
+            const password = argv.proxy_password;
+            const domain = argv.proxy_domain;
+            const port = argv.proxy_port;
+            return {
+                "server": `${protocol}://${domain}:${port}`,
+                "username": username,
+                "password": password
+            };
+        }
+    }
+});
+
+console.log("Browser cluster initialized");
+
+await cluster.task(processTurnstile);
+
+const app = express();
+
+app.get('/turnstile', queueTurnstile);
+
+app.get('/result', result);
+
+app.get('/', index);
+
+app.listen(argv.port, function () {
+    db.exec(`
+        CREATE TABLE IF NOT EXISTS turnstile_results (
+            id TEXT PRIMARY KEY NOT NULL,
+            status INTEGER NOT NULL CHECK (status IN (0, 1, 2)),
+            created_at TEXT NOT NULL,
+            proxy TEXT,
+            cf_clearance TEXT,
+            finished_at TEXT,
+            user_agent TEXT,
+            token TEXT
+        );
+    `);
+    console.log(`Server listening on port ${argv.port}`);
+});
+
+async function queueTurnstile(req, res) {
+    const url = req.query.url;
+    const sitekey = req.query.sitekey;
+
+    if (url === undefined || sitekey === undefined) {
+        await res.status(400).json({
+            msg: "'url' and 'sitekey' are required"
+        });
+    }
+
+    const id = crypto.randomUUID();
+
+    const insert = db.prepare(`
+        INSERT INTO turnstile_results (
+            id, status, created_at, proxy, cf_clearance,
+            finished_at, user_agent, token
+        ) VALUES (
+            @id, @status, @created_at, @proxy, @cf_clearance,
+            @finished_at, @user_agent, @token
+        )
+    `).run({
+        id: id,
+        status: 0, // queued
+        created_at: new Date().toISOString(),
+        proxy: null,
+        cf_clearance: null,
+        finished_at: null,
+        user_agent: null,
+        token: null
+    });
+
+    try {
+        await cluster.queue({url, sitekey, id}, undefined, {reject, resolve});
+    } catch (error) {
+        console.error(`Unexpected error queueing request: ${error.toString()}`);
+        await res.status(500).json({
+            msg: error.toString()
+        });
+    }
+
+    console.log(`Request queued with id: ${id}`);
+    await res.status(200).json({msg: id});
+}
+
+async function result(req, res) {
+    const id = req.query.id;
+
+    if (id === undefined) {
+        await res.status(400).json({
+            msg: "'id' is required"
+        });
+        return;
+    }
+
+    const result = db.prepare('SELECT * FROM turnstile_results WHERE id = ?').get(id);
+    if (result === undefined) {
+        await res.status(400).json({
+            msg:`"id ${id} not found`
+        });
+        return;
+    }
+
+    if (result.status === 0) {
+        await res.status(204).json({
+            msg: "in queue",
+        });
+        return;
+    } else if (result.status === 1) {
+        await res.status(200).json({
+            msg: "solved",
+            proxy: result.proxy,
+            cf_clearance: result.cf_clearance,
+            user_agent: result.user_agent,
+            token: result.token
+        });
+        return;
+    } else if (result.status === 2) {
+        await res.status(422).json({
+            msg: "failed"
+        });
+        return;
+    }
+
+    await res.status(500).json({
+        msg: "server error"
+    });
+}
+
+async function index(req, res) {
+    await res.status(200).send(`
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Turnstile Solver API</title>
+            <script src="https://cdn.tailwindcss.com"></script>
+        </head>
+        <body class="bg-gray-900 text-gray-200 min-h-screen flex items-center justify-center">
+            <div class="bg-gray-800 p-8 rounded-lg shadow-md max-w-2xl w-full border border-red-500">
+                <h1 class="text-3xl font-bold mb-6 text-center text-red-500">Welcome to Turnstile Solver API</h1>
+
+                <p class="mb-4 text-gray-300">To use the turnstile service, send a GET request to 
+                    <code class="bg-red-700 text-white px-2 py-1 rounded">/turnstile</code> with the following query parameters:</p>
+
+                <ul class="list-disc pl-6 mb-6 text-gray-300">
+                    <li><strong>url</strong>: The URL where Turnstile is to be validated</li>
+                    <li><strong>sitekey</strong>: The site key for Turnstile</li>
+                </ul>
+
+                <div class="bg-gray-700 p-4 rounded-lg mb-6 border border-red-500">
+                    <p class="font-semibold mb-2 text-red-400">Example usage:</p>
+                    <code class="text-sm break-all text-red-300">/turnstile?url=https://example.com&sitekey=sitekey</code>
+                </div>
+            </div>
+        </body>
+        </html>
+    `);
+}
+
+async function processTurnstile({page, data, worker}) {
+    const HTML_TEMPLATE = `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Turnstile Solver</title>
+        <script src="https://challenges.cloudflare.com/turnstile/v0/api.js" async></script>
+        <script>
+            async function fetchIP() {
+                try {
+                    const response = await fetch('https://api64.ipify.org?format=json');
+                    const data = await response.json();
+                    document.getElementById('ip-display').innerText = "Your IP: " + data.ip;
+                } catch (error) {
+                    console.error('Error fetching IP:', error);
+                    document.getElementById('ip-display').innerText = 'Failed to fetch IP';
+                }
+            }
+            window.onload = fetchIP;
+        </script>
+    </head>
+    <body>
+        <!-- cf turnstile -->
+        <p id="ip-display">Fetching your IP...</p>
+    </body>
+    </html>
+    `
+    const startTime = Date.now();
+
+    const id = data.id;
+    const url = data.url;
+    const sitekey = data.sitekey;
+    const { server, username, password } = worker.proxy;
+    const proxy = server.replace('://', `://${username}:${password}@`);
+
+    console.log(`Starting Turnstile solve for URL: ${url} with Sitekey: ${sitekey} and Proxy: ${proxy}`);
+    console.log(`Setting up page data and route`);
+
+    const urlWithSlash = url.endsWith("/") ? url : url + "/";
+    const turnstileDiv = `<div class="cf-turnstile" style="background: white;" data-sitekey="${sitekey}"></div>`;
+    const pageData = HTML_TEMPLATE.replace("<!-- cf turnstile -->", turnstileDiv);
+
+    await page.route(urlWithSlash, route => route.fulfill({ body: pageData, status: 200 }));
+    await page.goto(urlWithSlash);
+
+    console.log(`Setting up Turnstile widget dimensions`);
+
+    await page.locator("//div[@class='cf-turnstile']").evaluate(el => el.style.width = '70px');
+
+    console.log(`Starting Turnstile response retrieval loop`);
+
+    for (let i = 0; i < 10000; i++) {
+        try {
+            const turnstileCheck = await page.inputValue("[name=cf-turnstile-response]", { timeout: 5000 });
+            if (turnstileCheck === "") {
+                console.log(`Attempt ${i} - No Turnstile response yet`);
+                
+                await page.locator("//div[@class='cf-turnstile']").click({ timeout: 1000 });
+                await new Promise(resolve => setTimeout(resolve, 1000));
+            } else {
+                const elapsedTime = Math.round((Date.now() - startTime) / 1000 * 1000) / 1000;
+
+                console.log(`Successfully solved captcha - in ${elapsedTime} Seconds`);
+
+                const cookies = await page.context().cookies();
+                const cfClearance = cookies.find(x => x.name === "cf_clearance")?.value;
+                const userAgent = await page.evaluate(() => navigator.userAgent);
+
+                return { proxy, cfClearance, userAgent, turnstileCheck };
+            }
+        } catch {}
+    }
+
+    throw Error("Could not solve after looping many times")
+}
+
+function resolve(result) {
+    db.prepare(`
+        UPDATE turnstile_results
+        SET status = @status, finished_at = @finished_at, proxy = @proxy, cf_clearance = @cf_clearance, user_agent = @user_agent, token = @token
+        WHERE id = @id
+    `).run({
+        id: result.jobData.id,
+        status: 1, // success
+        finished_at: new Date().toISOString(),
+        proxy: result.data.proxy,
+        cf_clearance: result.data.cfClearance,
+        user_agent: result.data.userAgent,
+        token: result.data.turnstileCheck
+    });
+}
+
+function reject(result) {
+    db.prepare(`
+        UPDATE turnstile_results
+        SET status = @status, finished_at = @finished_at
+        WHERE id = @id
+    `).run({
+        id: result.jobData.id,
+        status: 2, // failed
+        finished_at: new Date().toISOString(),
+    });
+}
